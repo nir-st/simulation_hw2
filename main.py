cd9d0503afb792f129b3fd21a3c6dd6e2d361ab9
ostrovamit <82804444+ostrovamit@users.noreply.github.com>
2021-12-17 14:50:06 +0200

    amits func initial

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def print_amit(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 t toggle the breakpoint.
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
