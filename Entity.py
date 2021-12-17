6d8d3583fecab7de35cb9b547aa226430f303ab3
nir-st <nirstein@post.bgu.ac.il>
2021-12-17 14:44:21 +0200

    initial commit

---
+++ Entity.py
@@ -0,0 +1,18 @@
+class Entity:
+    def _init_(self, starting_position, velocity):
+        self.velocity = velocity
+        self.location = []
+
+    def _disired_location(self,x, y):
+        self._disired_location = (x, y)
+
+    def _movement(self):
+
+        if self.disired_location[0] > self.x:
+            self.x += 1
+        else:
+            self.x -= 1
+        if self.disired_location[1] > self.y:
+            self.y += 1
+        else:
+            self.y -= 1
