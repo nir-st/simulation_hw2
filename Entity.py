105290ae7e3e258c371e60016cca45b5f3bef430
Omer Marco <omerma3@gmail.com>
2021-12-17 18:15:28 +0200

    is_at_door

--- Entity.py
+++ Entity.py
@@ -4,6 +4,7 @@ class Entity:
         self.positions = []  # array of positions
         self.positions.append(starting_position)
 
+
     def get_desired_location(self, desired_direction):
         current_position = self.positions[len(self.positions) - 1]
         distance = self.velocity * 0.02
@@ -21,4 +22,13 @@ class Entity:
         if curr_position.distance_to(door_positions[0]) < curr_position.distance_to(door_positions[1]):
             return curr_position.direction_to(door_positions[0])
         return curr_position.direction_to(door_positions[1])
+
+    def is_at_door(self,doors):
+        last= len(self.positions)-1
+        curr_pos = self.get_position_at_k(self.positions[last])
+        for door in doors:
+            if door.distance_to(curr_pos)==0.0:
+                return True
+        return False
+
 
