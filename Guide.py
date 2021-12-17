14f92584c2dde5133dbef873319ff5888fcdfe94
nir-st <nirstein@post.bgu.ac.il>
2021-12-17 18:01:14 +0200

    guide has a field direction to door

--- Guide.py
+++ Guide.py
@@ -1,28 +1,20 @@
 import Entity
-import Position
 
 
 class Guide(Entity):
-    def __init__(self, starting_position, velocity, knows_left_door,knows_right_door):
+    def __init__(self, starting_position, velocity, knows_left_door):
         super().__init__(starting_position, velocity)
         self.knows_left_door = knows_left_door
-        self.knows_right_door = knows_right_door
-
+        self.direction_to_door = None
 
     def get_direction_to_door(self, room, k):
-        curr_possition = self.get_position_at_k(k)
+        curr_position = self.get_positions[0]
         door_positions = room.get_door_locations()
-
-        if self.knows_left_door and self.knows_right_door:
-
-            if curr_possition.distance_to(door_positions[0]) < curr_possition.distance_to(door_positions[1]):
-                return curr_possition.direction_to(door_positions[0])
-            return curr_possition.direction_to(door_positions[1])
-        elif self.knows_left_door:
-            return curr_possition.direction_to(door_positions[0])
+        if self.knows_left_door:
+            if curr_position.distance_to(door_positions[0]) < curr_position.distance_to(door_positions[1]):
+                direction_to_door = curr_position.direction_to(door_positions[0])
+            else:
+                direction_to_door = curr_position.direction_to(door_positions[1])
         else:
-            return curr_possition.direction_to(door_positions[1])
-
-
-# p = Position(1, 1)
-# g = Guide(p, 1, False)
+            direction_to_door = curr_position.direction_to(door_positions[0])
+        self.direction_to_door = direction_to_door
