543b70e62dca1e9d2b5a3580fa79c76ed1a7a77e
nir-st <nirstein@post.bgu.ac.il>
2021-12-17 15:37:00 +0200

    updated positions methods

--- Entity.py
+++ Entity.py
@@ -1,19 +1,10 @@
 class Entity:
-    def _init_(self, starting_position, velocity):
+    def __init__(self, starting_position, velocity):
         self.velocity = velocity
-        self.location = []
-        self.location[0] = starting_position
+        self.positions = []  # array of positions
+        self.positions[0] = starting_position
 
-    def _disired_location(self,x, y):
-        self._disired_location = (x, y)
-
-    def _movement(self):
-
-        if self.disired_location[0] > self.x:
-            self.x += 1
-        else:
-            self.x -= 1
-        if self.disired_location[1] > self.y:
-            self.y += 1
-        else:
-            self.y -= 1
+    def get_desired_location(self, desired_direction):
+        current_position = self.locations[len(self.positions) - 1]
+        distance = self.velocity * 0.02
+        return current_position.move_to(desired_direction, distance)
--- Position.py
+++ Position.py
@@ -2,9 +2,24 @@ import math
 
 
 class Position:
-    def _init_(self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def distance_to(self, other_position):
         return math.sqrt(math.pow(self.x - other_position.x, 2) + math.pow(self.y - other_position.y, 2))
+
+    def direction_to(self, other_position):
+        # returns degree
+        deltaX = other_position.x - self.x
+        deltaY = other_position.y - self.y
+        return math.atan2(deltaX, deltaY) / math.pi * 180
+
+    def move_to(self, direction, distance):
+        new_x = self.x + distance * math.cos(direction * math.pi / 180)
+        new_y = self.y + distance * math.sin(direction * math.pi / 180)
+        return Position(new_x, new_y)
+
+    def __repr__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
