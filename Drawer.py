fb2879b5580e469d397dcf909d9580fee26f99a5
nir-st <nirstein@post.bgu.ac.il>
2021-12-18 23:06:10 +0200

    updated drawer

--- Drawer.py
+++ Drawer.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-def draw_shit(guides_posistions, entitys_positions):
+def draw_routes(guides_posistions, entitys_positions):
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
+
+    ax1.scatter([20], [10], c="b", marker="s")
 
     for gp in guides_posistions:
         guide_xs = []
@@ -12,7 +14,6 @@ def draw_shit(guides_posistions, entitys_positions):
         for pos in gp:
             guide_xs.append(pos.x)
             guide_ys.append(pos.y)
-        # ax1.scatter(guide_xs, guide_ys, c="y", marker=".", label='guide')
         ax1.scatter(guide_xs, guide_ys, c="y", marker=".")
 
     for ep in entitys_positions:
@@ -21,9 +22,11 @@ def draw_shit(guides_posistions, entitys_positions):
         for pos in ep:
             entity_xs.append(pos.x)
             entity_ys.append(pos.y)
-        ax1.scatter(entity_xs, entity_ys, c="r", marker=".", label='regular')
+        ax1.scatter(entity_xs, entity_ys, c="r", marker=".")
 
-    # plt.legend(loc='best')
+    plt.xlabel('X coordinate')
+    plt.ylabel('Y coordinate')
+    plt.title("Guides Route")
     plt.xlim([0, 20])
     plt.ylim([0, 20])
     plt.show()
